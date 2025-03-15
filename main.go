fbd1c5540e20f6dfdb76e0ec970685b092e45bac
f6o <25875630+f6o@users.noreply.github.com>
2025-03-15 20:30:43 +0900

    use env value for server

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"github.com/f6o/memoapp/proto"
 	"github.com/f6o/memoapp/server"
@@ -10,13 +11,31 @@ import (
 	"google.golang.org/grpc"
 )
 
+const (
+	defaultDbFile        = "memos.db"
+	defaultListenAddress = ":50051"
+)
+
 func main() {
+	dbFile := os.Getenv("DB_FILE")
+	if dbFile == "" {
+		dbFile = defaultDbFile
+	}
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	if listenAddr == "" {
+		listenAddr = defaultListenAddress
+	}
+	if _, _, err := net.SplitHostPort(listenAddr); err != nil {
+		log.Fatalf("invalid listen address: %v", err)
+	}
+
 	repo, err := repository.NewMemoRepository("memos.db")
 	if err != nil {
 		log.Fatalf("failed to create repository: %v", err)
 	}
 
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
