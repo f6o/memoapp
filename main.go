63d9701b84e2a2cbef4cae03c9d73d751c31e03e
f6o <25875630+f6o@users.noreply.github.com>
2025-03-15 19:11:25 +0900

    start server

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"github.com/f6o/memoapp/proto"
+	"github.com/f6o/memoapp/repository"
+	"github.com/f6o/memoapp/server"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	fmt.Println("Hello from Memo App!")
+	repo, err := repository.NewMemoRepository("memos.db")
+	if err != nil {
+		log.Fatalf("failed to create repository: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	proto.RegisterMemoServiceServer(s, server.NewServer(repo))
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
